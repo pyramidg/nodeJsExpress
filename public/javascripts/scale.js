8fde834c71a7ac7213371ef0f76dcfc49a53298c
studioplace <webstudioplace.com>
2016-11-17 16:40:19 -0500

    Javascript animation with intro text

--- public/javascripts/scale.js
+++ public/javascripts/scale.js
@@ -38,15 +38,26 @@ $('.navbar-collapse ul li a').click(function() {
 
 
 
-TweenMax.to(".brand-heading", 6,{
+TweenMax.to(".brand-heading", 3,{
   left:600, backgroundColor:"black",
   padding:20,
   borderColor:"blue",
   borderRadius:26,
 
  });
- TweenMax.to(".brand-heading", 6,{
+ TweenMax.to(".brand-heading", 3,{
    x:0,  rotation:360,
    scale:0.5, ease: Back.easeInOut.config(1.7), y: 0 ,
    opacity:0,
    });
+
+
+  TweenMax.staggerFrom(".intro-text" , 1, { delay:2, scale:0,}, 1);
+
+
+  TweenMax.staggerFrom(".intro-text2", 1, { delay:2.5, scale:0}, 1);
+
+
+  $(".btn").click(function(){
+    TweenMax.staggerTo(".intro-text , .intro-text2", 0.5, {opacity:0, y:-100, ease:Back.easeIn}, 0.1);
+  });
